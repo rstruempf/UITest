2a5a3d295b614499a74f06acd9c6ffcacc4e6948
Ron Struempf <rstruem1@my.westga.edu>
2016-01-30 17:16:50 -0500

    Added fragment id generation, plus save/restore state

--- app/src/main/java/com/ronstruempf/uitest/aFragment.java
+++ app/src/main/java/com/ronstruempf/uitest/aFragment.java
@@ -3,6 +3,7 @@ package com.ronstruempf.uitest;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,19 +12,38 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
+ * Playing with Fragments
+ *
  * Created by Ron on 1/21/2016.
  */
 public class aFragment extends Fragment {
 
+    private final String APP_TAG = "aFragment_Debug";
+    private final String SAVE_ID = "aFragment_Id";
+    private final String SAVE_TEXT = "aFragment_Text";
+
+    private static int nextId = 110;
+    private int ourId;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        LinearLayout linLayout=
-                new LinearLayout(getActivity());
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
+        LinearLayout linLayout = new LinearLayout(getActivity());
         linLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-
         final TextView tv=new TextView(getActivity());
-        tv.setText("Text Entry");
+        if (state != null) {
+            ourId = state.getInt(SAVE_ID);
+            if (ourId >= nextId) {
+                nextId = ourId + 1;
+            }
+            tv.setText(state.getCharSequence(SAVE_TEXT));
+            Log.d(APP_TAG, "onCreateView: Restoring id=" + ourId + ", text=" + tv.getText());
+        }
+        else {
+            ourId = nextId++;
+            tv.setText("Text Entry");
+            Log.d(APP_TAG, "onCreateView: New id=" + ourId + ", text=" + tv.getText());
+        }
+        tv.setId(ourId);
         linLayout.addView(tv);
         View.OnClickListener onclick=
                 new View.OnClickListener(){
@@ -46,5 +66,17 @@ public class aFragment extends Fragment {
         return linLayout;
     }
 
-
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putInt(SAVE_ID, ourId);
+        View vw = getView();
+        if (vw == null) {
+            Log.e(APP_TAG, "Error, getView() returned null on onSaveInstanceState()");
+            return;
+        }
+        TextView tv = (TextView)vw.findViewById(ourId);
+        state.putCharSequence(SAVE_TEXT, tv.getText());
+        Log.d(APP_TAG, "onSaveInstanceState: Saving id=" + ourId + ", text=" + tv.getText());
+    }
 }
